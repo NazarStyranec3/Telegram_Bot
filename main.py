8dff1c18f34393023372bfb0b9cd6b0b6375d9e9
NazarStyranec3 <nazarsturanec@gmail.com>
2025-09-17 02:40:12 +0300

    start

---
+++ main.py
@@ -0,0 +1,35 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("7086342977:AAE7pASZOadINLnmt_gCxZfwMQYDwwfRM04")
+
+# стартова команда
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(
+        types.InlineKeyboardButton("1", callback_data="1"),
+        types.InlineKeyboardButton("2", callback_data="2"),
+        types.InlineKeyboardButton("3", callback_data="3")
+    )
+    bot.send_message(message.chat.id, "Вибери цифру:", reply_markup=markup)
+
+
+# обробка натискань
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data in ["1", "2", "3"]:
+        markup = types.InlineKeyboardMarkup()
+        for i in range(1, 4):
+            markup.add(types.InlineKeyboardButton(f"{call.data}{i}", callback_data=f"{call.data}{i}"))
+        bot.edit_message_text(
+            f"Ти вибрав {call.data}. Обери далі:",
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=markup
+        )
+    elif len(call.data) == 2:
+        bot.send_message(call.message.chat.id, f"Ти вибрав фінальний варіант: {call.data}")
+
+
+bot.polling(non_stop=True)
